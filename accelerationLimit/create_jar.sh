d2a5d45782ca52ef42e5d9faf26f0b8d428f3d88
ahmed-wael2002 <ahmedwael978@gmail.com>
2025-06-01 17:31:12 +0300

    created automation script

--- accelerationLimit/create_jar.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Compiling scala file
-scalac -cp tpdejavu.jar output/TraceMonitor.scala 2>&1 -d TraceMonitor.jar
-# Updating jar file 
-jar -uvfe TraceMonitor.jar TraceMonitor.class
---
+++ accelerationLimit/test_acceleration_limit.py
@@ -0,0 +1,16 @@
+import jpype
+import jpype.imports
+
+import time
+
+jpype.startJVM(jpype.getDefaultJVMPath(), "-Djava.class.path=tpdejavu.jar:TraceMonitor.jar")
+
+monitor = jpype.JClass("acceleration_limit.TraceMonitor")
+
+file = open("log.csv")
+while True:
+    line = file.readline()
+    if not line:
+        break
+    line = line.strip()
+    monitor.eval(line)
